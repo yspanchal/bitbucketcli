b272ed23539d025f1c94609fee0cf32dc6e2336b
Jan Vlčinský <jan.vlcinsky@tamtamresearch.com>
2015-06-14 23:53:26 +0200

    test failing on stdin/out encoding rewritten (test_user_info.py)
            deleted:    tests/test_cat.py
            modified:   tests/test_user_info.py

--- tests/test_user_info.py
+++ tests/test_user_info.py
@@ -3,11 +3,11 @@ import subprocess
 
 def test_it(binbb, account):
     """Test just the first one"""
-    expected_words = ("Fields Value username first_name last_name display_name"
-                      "is_team is_staff").split()
+    expected_words = ("Field Value username first_name last_name display_name"
+                      " is_team is_staff").split()
     bbcmd = ["user", "info"]
     # Expecting the call to succeed
-    #res = binbb.sysexec(*bbcmd)
+    # res = binbb.sysexec(*bbcmd)
     cmd = [binbb.strpath] + bbcmd
     res = subprocess.check_output(cmd)
 
